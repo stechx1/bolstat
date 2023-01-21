ddea756df9697e70dae67e8445cdaebb0aa52f3b
Summar <summarkimail@gmail.com>
2023-01-21 22:13:02 +0500

    Integrated firebase and app functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar/Navbar'
-import '../styles/globals.css'
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar/Navbar";
+import "../styles/globals.css";
+import "../utiils/firebase";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "../context/auth-context";
 
 function MyApp({ Component, pageProps }) {
-  return <> <Navbar/> <Component {...pageProps} /> <Footer/> </>
+  return (
+    <>
+      <AuthProvider>
+        <Navbar /> <Component {...pageProps} /> <Footer />{" "}
+        <ToastContainer autoClose={2000} />
+      </AuthProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
